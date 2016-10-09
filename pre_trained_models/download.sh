441e7db33c57900ab7f6ad5fe9177f6e44054b92
Ravi Teja Mullapudi <raviteja.mullapudi@gmail.com>
2016-10-09 17:50:25 -0400

    Adding download script for pretrained models.

---
+++ pre_trained_models/download.sh
@@ -0,0 +1,8 @@
+wget http://download.tensorflow.org/models/inception_v1_2016_08_28.tar.gz
+wget http://download.tensorflow.org/models/inception_v2_2016_08_28.tar.gz
+wget http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz
+wget http://download.tensorflow.org/models/resnet_v1_50_2016_08_28.tar.gz
+wget http://download.tensorflow.org/models/resnet_v1_101_2016_08_28.tar.gz
+wget http://download.tensorflow.org/models/resnet_v1_152_2016_08_28.tar.gz
+wget http://download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz
+wget http://download.tensorflow.org/models/vgg_19_2016_08_28.tar.gz
